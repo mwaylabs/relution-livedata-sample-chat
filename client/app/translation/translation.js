2bff4722c38d7ef61dbf4f6542e90c211c4bfd08
DrMabuse23 <p.brewing@mwaysolutions.com>
2015-07-03 11:29:28 +0200

    Update 2015-07-03T09:29:16.563Z

--- client/app/translation/translation.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-angular.module('translation', [
-  'pascalprecht.translate',
-  'ngCordova',
-  'translation.service'
-]);
---
+++ js/docs-setup.js
@@ -0,0 +1,176 @@
+NG_DOCS={
+  "sections": {
+    "api": "Relutionlivedata Sample Chat - 0.0.1"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "about",
+      "shortName": "about",
+      "type": "interface",
+      "moduleName": "about",
+      "shortDescription": "the about module",
+      "keywords": "api interface module"
+    },
+    {
+      "section": "api",
+      "id": "about:StartCtrl",
+      "shortName": "StartCtrl",
+      "type": "controller",
+      "moduleName": "about:StartCtrl",
+      "shortDescription": "add your description",
+      "keywords": "$scope add api controller description"
+    },
+    {
+      "section": "api",
+      "id": "auth",
+      "shortName": "auth",
+      "type": "interface",
+      "moduleName": "auth",
+      "shortDescription": "the auth module",
+      "keywords": "api interface module"
+    },
+    {
+      "section": "api",
+      "id": "auth:LoginCtrl",
+      "shortName": "LoginCtrl",
+      "type": "controller",
+      "moduleName": "auth:LoginCtrl",
+      "shortDescription": "add your description",
+      "keywords": "$scope add alert api build controller description error form getmessage loader login message method property relution-client-security service smal small submit submitted user valid window"
+    },
+    {
+      "section": "api",
+      "id": "main",
+      "shortName": "main",
+      "type": "interface",
+      "moduleName": "main",
+      "shortDescription": "the common module",
+      "keywords": "api common interface main module"
+    },
+    {
+      "section": "api",
+      "id": "main:AlertService",
+      "shortName": "AlertService",
+      "type": "service",
+      "moduleName": "main:AlertService",
+      "shortDescription": "add your description",
+      "keywords": "add api description main service"
+    },
+    {
+      "section": "api",
+      "id": "main:hourGlassLoader",
+      "shortName": "hourGlassLoader",
+      "type": "directive",
+      "moduleName": "main:hourGlassLoader",
+      "keywords": "api directive main"
+    },
+    {
+      "section": "api",
+      "id": "messenger",
+      "shortName": "messenger",
+      "type": "interface",
+      "moduleName": "messenger",
+      "shortDescription": "the messenger module",
+      "keywords": "api interface messenger module"
+    },
+    {
+      "section": "api",
+      "id": "messenger:ChatCtrl",
+      "shortName": "ChatCtrl",
+      "type": "controller",
+      "moduleName": "messenger:ChatCtrl",
+      "keywords": "$filter $ionicloading $ionicscrolldelegate $q $rootscope $scope $state $stateparams $timeout $window _addeventlistener _list add addmessage api audio bottom changeread chat checks collection controller debug debugging delegat-handle dong erroronmessage events field form gongmessage input isread js list load loginservice main mediaplay message messages messenger method model mp3 partner play property receiver relutionlivedata remove save script scroll scrollbottom send sender server settings start toggle true user wil wrong"
+    },
+    {
+      "section": "api",
+      "id": "messenger:Conversation",
+      "shortName": "Conversation",
+      "type": "service",
+      "moduleName": "messenger:Conversation",
+      "shortDescription": "get and return the chat conversation from a user and a receiver",
+      "keywords": "api chat collection conversation messenger model property receiver relutionlivedata return service user"
+    },
+    {
+      "section": "api",
+      "id": "messenger:MessagesCtrl",
+      "shortName": "MessagesCtrl",
+      "type": "controller",
+      "moduleName": "messenger:MessagesCtrl",
+      "keywords": "$q $scope $state api changeaudiosetting chatting controller fo groupusers inocming loginservice media message messageservice messenger method property ready send service settings sound toggle usermessengerservice users wil"
+    },
+    {
+      "section": "api",
+      "id": "messenger:UserMessengerService",
+      "shortName": "UserMessengerService",
+      "type": "service",
+      "moduleName": "messenger:UserMessengerService",
+      "shortDescription": "get the available Chat partner for your user",
+      "keywords": "$filter $rootscope $state api chat collection collectiontype conversationsuser currentuser error fetch filter filterbyuuid getuserbyuuid group handleerror https loggedin main messenger model models mozilla org partner property relutionlivedata return service user users userservice uuid"
+    },
+    {
+      "section": "api",
+      "id": "translation",
+      "shortName": "translation",
+      "type": "interface",
+      "moduleName": "translation",
+      "shortDescription": "the translation module",
+      "keywords": "api interface module translation"
+    },
+    {
+      "section": "api",
+      "id": "translation:TranslationSelectItem",
+      "shortName": "TranslationSelectItem",
+      "type": "directive",
+      "moduleName": "translation:TranslationSelectItem",
+      "shortDescription": "plz define in your config the following LANGUAGES: [&#39;de-DE&#39;, &#39;en-IN&#39;] and in your translation Files &#39;LANGUAGES&#39; key",
+      "keywords": "$filter $translate api box change changelanguage class config de-de define deutsch directive en-in files getlanguagemenu key label langsuffix language languagemenu languages list map menu method plz property select set setselectedlanguage translation"
+    },
+    {
+      "section": "api",
+      "id": "translation:TranslationService",
+      "shortName": "TranslationService",
+      "type": "service",
+      "moduleName": "translation:TranslationService",
+      "shortDescription": "preload translations",
+      "keywords": "$http api array default defaulttranslation file folder getjson getpath i18n json key languages load method path preload prepare promise promiselanguage promises property qall retsurn return saved service set setdefaulttranslationkey storage translation translations"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "__file": "_FAKE_DEST_/js/docs-setup.js",
+  "__options": {
+    "startPage": "/api",
+    "scripts": [
+      "js/angular.min.js",
+      "js/angular-animate.min.js",
+      "js/marked.js",
+      "js/prettify.js"
+    ],
+    "styles": [],
+    "title": "Relutionlivedata Sample Chat - 0.0.1",
+    "html5Mode": true,
+    "editExample": true,
+    "navTemplate": false,
+    "navContent": "",
+    "navTemplateData": {},
+    "image": "http://www.relution.io/de/wp-content/themes/Divi/images/logo.png",
+    "imageLink": "https://github.com/mwaylabs/relution-livedata-sample-chat",
+    "loadDefaults": {
+      "angular": true,
+      "angularAnimate": true,
+      "marked": true,
+      "prettify": true
+    }
+  },
+  "html5Mode": true,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js",
+    "angular-animate.min.js",
+    "marked.js",
+    "prettify.js"
+  ]
+};
